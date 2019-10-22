95e6a063d452233e44addc51095066e34188a577
1136863240 <1136863240@qq.com>
2019-10-22 17:06:47 +0800

    class4

---
+++ source/class4/shizhan.py
@@ -0,0 +1,5 @@
+yashua = 19.99
+lingshi = 8
+qingcai = 1.99
+sum = yashua * 3 + lingshi * 2 + qingcai
+print(sum)
---
+++ source/class4/yunsuanfu.py
@@ -0,0 +1,21 @@
+print(1 + 1)
+print(1 - 1)
+print(1 * 1)
+print(1 / 1)
+print(2 ** 3)
+print(8 // 4)
+
+print()
+
+print(1 == 1)
+print(1 == 2)
+print(1 != 2)
+print(1 <= 2)
+print(1 >= 2)
+
+a = 1
+b = 2
+print()
+
+a += 2
+print(a)
